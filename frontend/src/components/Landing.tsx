e04d5a400ba8b22f3afedd49d477f7d36cee8a15
vibhu/1805 <vibhutisharma483@gmail.com>
2024-01-17 01:54:06 +0530

    frontend started

---
+++ frontend/src/components/Landing.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import {Link} from "react-router-dom";
+export const Landing = () => {
+    const [name,setName]=useState("");
+   // const history= useHis
+    return <div>
+        <input type="text" onChange={(e) => {
+            setName(e.target.value);
+        }}>
+        </input>
+        <Link to={`/room/?name=${name}`}>Join</Link>
+    </div>
+}
---
+++ frontend/src/components/Room.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+export const Room = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const name = searchParams.get('name');
+    useEffect(() =>{
+        //logic to init user to room
+        
+    }, [name])
+    return <div>
+        Hi {name}
+    </div>
+}
